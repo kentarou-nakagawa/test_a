de283ed9ef338f03327532d3cd97f98863322967
kentarou-nakagawa <n.kentarou1205@icloud.com>
2023-03-05 00:41:39 +0900

    デフォルト

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleClick = () => {
+    axios.get('https://catfact.ninja/fact')
+      .then(response => {
+        setText(response.data.fact);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{text}</p>
+      <button onClick={handleClick}>更新</button>
     </div>
   );
 }
 
 export default App;
+
